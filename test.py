9fb5c49ede0e658abf6f43008836e683b383ac5e
jcliedu <52899143+jcliedu@users.noreply.github.com>
2020-10-19 17:29:45 +0800

    update

--- test.py
+++ test.py
@@ -1,6 +1,92 @@
 import dpkt
+from math import log
+
+def calculate(a, b):
+    def a2b(num):
+        bit = list(bin(num)[2:])
+        if len(bit) != 8:
+            tem = 8 - len(bit)
+            while tem > 0:
+                bit.insert(0, '0')
+                tem -= 1
+            return bit
+        else:
+            return bit
+
+    byte_a = a2b(a)
+    byte_b = a2b(b)
+
+    # print(byte_a)
+    # print(len(byte_a))
+    # print(byte_b)
+    # print(len(byte_b))
+
+    same_num = 0
+    for i in range(0, 8):
+        if byte_a[i] == byte_b[i]:
+            same_num += 1
+        else:
+            continue
+
+    return same_num/8
+
+datapath = './data_smtp/'
+
+i = 1
+while 1:
+    try:
+        f = open(datapath + str(i) + '.pcap', 'rb')
+        pcap = dpkt.pcap.Reader(f)
+
+        for ts, buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            tcp = ip.data
+            data = tcp.data
+            print(data)
+
+            BC = []
+            for j in range(0, len(data)-1):
+                BC.append(calculate(data[j], data[j+1]))
+            print(BC)
+
+            deltaBC = []
+            for j in range(0, len(BC)-1):
+                deltaBC.append((BC[j+1] - BC[j]))
+            print(deltaBC)
+
+            vote = [0]*len(data)
+            for j in range(len(deltaBC)-1):
+                if j == 0:
+                    vote[j] += 1
+                elif deltaBC[j] < deltaBC[j-1] and deltaBC[j] < deltaBC[j+1]:
+                    vote[j+1] += 1
+
+            # j = 0
+            # temp_data = b''
+            # while j < len(data):
+            #     if vote[j] == 1 and data[j] == 32:
+            #         temp_data += b'^'
+            #     else:
+            #         temp_data += chr(data[j]).encode()
+            #     j += 1
+
+            # delimiters = [' ', ':', '\r', '\n']
+            # for j in range(len(data)):
+            #     if chr(data[j]) in delimiters:
+            #         vote[j] += 3
+
+            print(data)
+            print(len(data))
+            print(vote)
+            print(len(vote))
 
 
+    except:
+        print("\n[error] Process break abnormally.")
+        print("Something wrong with Packet num-{0}, lost or error".format(i))
+        break
 
-
-print("-------")
+    i += 1
+    if i > 10:
+        break
