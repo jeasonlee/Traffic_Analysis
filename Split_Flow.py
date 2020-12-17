9f2269eaa3ba9ac6c2dd0e092c892d6c7052d645
jcliedu <52899143+jcliedu@users.noreply.github.com>
2020-12-17 20:46:36 +0800

    update

--- Split_Flow.py
+++ Split_Flow.py
@@ -71,3 +71,16 @@ if __name__ == '__main__':
     print("-" * 40)
     for key, value in udp_flow_dict.items():
         print("ip.src:{0}, ip.dst:{1}, src_port:{2}, dst_port:{3} data:{4}".format(socket.inet_ntoa(key.src_ip), socket.inet_ntoa(key.dst_ip), key.src_port, key.dst_port, len(value)))
+
+    result_path = os.path.join('', "result")
+    if not os.path.exists(result_path):
+        os.mkdir(result_path)
+
+    for i, value in zip(range(len(tcp_flow_dict.values())), tcp_flow_dict.values()):
+        flow_new = open(os.path.join(result_path, str(i) + "_tcp.pcap"), 'wb')
+        writer = dpkt.pcap.Writer(flow_new)
+        value.sort(key = lambda x:x[0], reverse=False)
+        for pkt in value:
+
+            writer.writepkt(pkt = pkt[1], ts = pkt[0])
+        flow_new.close()
