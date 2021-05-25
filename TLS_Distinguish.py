9729f6e2a6833ebabbf3b168e0fbb87c72b56ce8
jcliedu <52899143+jcliedu@users.noreply.github.com>
2021-05-25 19:30:29 +0800

    update

--- TLS_Distinguish.py
+++ TLS_Distinguish.py
@@ -4,9 +4,27 @@ import os
 tls_path = 'D:/tls'
 file_name_list = os.listdir(tls_path)
 for file_name in file_name_list:
+    file_path = os.path.join(tls_path, file_name)
+    print(file_path)
     try:
-        file_path = os.path.join(tls_path, file_name)
-        print(file_path)
         f = open(file_path, 'rb')
+        pcap = dpkt.pcap.Reader(f)
+        for ts, buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            tcp = ip.data
+            data = tcp.data
+            if len(data) != 0:
+                if data[0] == 22:
+                    print(len(data))
+                    if data[5] == 1:
+                        print("Client Hello!")
+                    elif data[5] == 2:
+                        print("Server Hello!")
+                    elif data[5] == 11:
+                        print("Certificate!")
+                elif data[0] == 23:
+                    print("Application Data!")
+
     except Exception as e:
         print(e)
