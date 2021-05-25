87d69aff23742ba8bde95598cf5455f7e6c71227
jcliedu <52899143+jcliedu@users.noreply.github.com>
2021-05-25 11:23:03 +0800

    update

--- TLS_Distinguish.py
+++ TLS_Distinguish.py
@@ -1,2 +1,12 @@
 import dpkt
 import os
+
+tls_path = 'D:/tls'
+file_name_list = os.listdir(tls_path)
+for file_name in file_name_list:
+    try:
+        file_path = os.path.join(tls_path, file_name)
+        print(file_path)
+        f = open(file_path, 'rb')
+    except Exception as e:
+        print(e)
